024948185b6236cca0eea2384b096e748d43f2b9
Pekka Lukkari <pekka_lukkari@hotmail.com>
2015-02-06 15:56:09 +0200

    Javascript code made more  'modular'

--- CalculatorApp_vanha/calculator-app.js
+++ CalculatorApp_vanha/calculator-app.js
@@ -1,306 +1,353 @@
 ﻿/// <reference path="C:\Files\Source\Repos\CalculatorApp_vanha\Scripts/jquery-1.11.1.intellisense.js" />
 /// <reference path="C:\Files\Source\Repos\CalculatorApp_vanha\Scripts/jquery-1.11.1.js" />
-/// <reference path="C:\Files\Source\Repos\CalculatorApp_vanha\Scripts/jspdf/jspdf.source.js" />
 /// <reference path="C:\Files\Source\Repos\CalculatorApp_vanha\Scripts/jquery-ui-1.11.2.js" />
+/// <reference path="C:\Files\Source\Repos\CalculatorApp_vanha\Scripts/jspdf/jspdf.source.js" />
 
 $(document).ready(function () {
     CalculatorInitialize();
 });
 
 function CalculatorInitialize() {
-    $('#btnClearCharacter').on("click", ClearCharacter);
-    $('#btnClearAll').on("click", ClearAll);
-    $('#btnChangeSign').on("click", ChangeNumberSign);
-    AddNumberEventListeners();
-    AddOperatorEventListeners();
-    $('#btnComma').on("click", AddComma);
-    $('#btnTotal').on("click", CalculateTotal);
-
-    SetCalculation(0);
-    SetResult(0);
-
-    $('#btnAddComment').on("click", AddComment);
-    $('#btnClearResultsList').on("click", ClearResultsList);
-    $('#btnPrint').on("click", PrintCalculations);
-    $('#btnPDF').on("click", DownloadPDF);
-
-    $("#sortable").sortable({
-        placeholder: "ui-state-highlight"
-    });
-    $("#sortable").disableSelection();
+    var UI = new CalculatorUI();
+    var Functions = new CalculatorFunctions(UI);
+    var eventListeners = new CalculatorEventListeners(UI, Functions);
+    eventListeners.AddAll();
+    
+    UI.SetCalculation(0);
+    UI.SetResult(0);   
 }
 
-function GetCalculation() {
-    return $('#txtCalculation').val();
-}
+var CalculatorEventListeners = function(UI, Functions) {
 
-function GetLastCharacter(string) {
-    return string[string.length - 1];
-}
+    this.AddAll = function () {
+        /* Calculator funtions event listeners */
+        $('#btnClearCharacter').on("click", Functions.ClearCharacter);
+        $('#btnChangeSign').on("click", Functions.ChangeNumberSign);
+        $('#btnComma').on("click", Functions.AddComma);
+        $('#btnTotal').on("click", Functions.CalculateTotal);
+        AddNumberEventListeners();
+        AddOperatorEventListeners();
 
-function IsLastCharacterOperator(string) {
-    if (IsOperator(GetLastCharacter(string))) 
-        return true;
-    return false;
-}
+        /* Calculator UI interface event listeners */
+        $('#btnClearAll').on("click", UI.ClearAll);
+        $('#btnAddComment').on("click", UI.AddComment);
+        $('#btnClearResultsList').on("click", UI.ClearResultsList);
+        $('#btnPrint').on("click", UI.PrintCalculations);
+        $('#btnPDF').on("click", UI.DownloadPDF);
 
-function SetCalculation(calculation) {
-    $('#txtCalculation').val(calculation);
-}
-
-function SetResult(result) {
-    $('#txtResult').val(result);
-}
-
-function AddCalculationAsResult(result) {
-    var lasku = GetCalculation();
-    $('#txtResult').val(result);
-
-    var lisattava = '<li><div>' + lasku + ' = ' + result + '</div><div class="ui-icon ui-icon-closethick" onclick="RemoveCalculation(event);"></div><div></div></li>'
-    $('#sortable').append(lisattava)
-
-    SetCalculation(0);
-}
-
-function CalculateTotal() {
-    var allCalculations = GetCalculation();
-    var numberOfCalculations = CountNumberOfCalculations(allCalculations);
-
-    if (numberOfCalculations === 0)
-        return;
-
-    for (var i = 0; i < numberOfCalculations; i++) {
-        var calculation = TakeCalculation(allCalculations);
-        var result = ExecuteCalculation(calculation);
-        allCalculations = ReplaceCalculationWithResult(allCalculations, calculation, result);
+        /* JQuery UI sortable component initialization */
+        $("#sortable").sortable({
+            placeholder: "ui-state-highlight"
+        }).disableSelection();
     }
 
-    AddCalculationAsResult(result);
-}
-
-function ReplaceCalculationWithResult(allCalculations, calculation, result) {
-    var poistettavaSetti = calculation["firstNumber"].toString().length +
-                           calculation["operator"].toString().length +
-                           calculation["secondNumber"].toString().length;
-    var newCalculation = allCalculations.substring(poistettavaSetti, allCalculations.length);
-    return result + newCalculation;
-}
-
-
-function CountNumberOfCalculations(allCalculations) {
-    var numberOfCalculations = 0;
-    for (var i = 0; i < allCalculations.length; i++) {
-        if (IsOperator(allCalculations[i])) {
-            numberOfCalculations++;
-        }
-    }
-    return numberOfCalculations;
-}
-
-function TakeCalculation(allCalculations) {
-    var calculation = new Array(3);
-    var firstOperatorIndex = 0;
-
-    for (var i = 0; i < allCalculations.length; i++) {
-        if (IsOperator(allCalculations[i])) {
-            if (firstOperatorIndex === 0) {
-                calculation["firstNumber"] = Number(allCalculations.substring(0, i));
-                calculation["operator"] = allCalculations[i];
-                firstOperatorIndex = i;
-            } else {
-                calculation["secondNumber"] = Number(allCalculations.substring(firstOperatorIndex + 1, i));
-                break;
-            }
-        }
-    }
-  
-    if (calculation["secondNumber"] === undefined) {
-        calculation["secondNumber"] = Number(allCalculations.substring(firstOperatorIndex + 1, i));
-    }
-
-    return calculation;
-}
-
-function ExecuteCalculation(calculation) {
-    var operator = calculation["operator"];
-    var firstNumber = Number(calculation["firstNumber"]);
-    var secondNumber = Number(calculation["secondNumber"]);
-
-    switch (calculation["operator"]) {
-        case '+':
-            return firstNumber + secondNumber;
-            break;
-        case '-':
-            return firstNumber - secondNumber;
-            break;
-        case '*':
-            return firstNumber * secondNumber;
-            break;
-        case '/':
-            return firstNumber / secondNumber;
-            break;
-    }
-}
-
-function ClearCharacter() {
-    var calculation = GetCalculation();
-
-    if (calculation.length === 0 || calculation.length === 1) {
-        SetCalculation(0);
-    } else {
-        var newCalculation = calculation.substring(0, calculation.length - 1);
-
-        if (IsLastCharacterOperator(newCalculation)) {
-            newCalculation = calculation.substring(0, newCalculation.length - 1);
-        }
-
-        SetCalculation(newCalculation);
-    }
-}
-
-function ClearAll() {
-    SetCalculation(0);
-    SetResult(0);
-}
-
-function ChangeNumberSign() {
-    var calculation = GetCalculation();
-    var operatorPosition = CheckFirstOperatorPosition(calculation);
-
-    if (operatorPosition === 0) {
-        SetCalculation(-parseFloat(calculation));
-    }
-}
-
-function CheckFirstOperatorPosition(calculation) {
-    var operatorPositions = [];
-
-    for (var i = 0; i < calculation.length; i++) {
-        if (IsOperator(calculation[i])) {
-            operatorPositions.push(i);
+    var AddNumberEventListeners = function() {
+        for (var i = 0; i < 10; i++) {
+            (function (j) {
+                var btnNumber = "#btn" + j;
+                $(btnNumber).on("click", function () { Functions.AddNumber(j); });
+            })(i);
         }
     }
 
-    if (operatorPositions.length === 0) {
-        return 0;
-    } else {
-        return operatorPositions[operatorPositions.length-1]
-    }  
-}
-
-function IsOperator(character) {
-    if (character == '+' ||
-        character == '-' ||
-        character == '*' ||
-        character == '/') {
-        return true;
+    var AddOperatorEventListeners = function () {
+        $('#btnSum').on("click", function () { Functions.AddOperator('+') })
+        $('#btnSubstract').on("click", function () { Functions.AddOperator('-') })
+        $('#btnMultiply').on("click", function () { Functions.AddOperator('*') })
+        $('#btnDivide').on("click", function () { Functions.AddOperator('/') })
     }
-    return false;
+
 }
 
-function AddNumberEventListeners() {
-    for (var i = 0; i < 10; i++) {
-        (function (j) {
-            var btnNumber = "#btn" + j;
-            $(btnNumber).on("click", function () { AddNumber(j); });
-        })(i);
-    }
-}
-
-function AddOperatorEventListeners() {
-    $('#btnSum').on("click", function () { AddOperator('+') })
-    $('#btnSubstract').on("click", function () { AddOperator('-') })
-    $('#btnMultiply').on("click", function () { AddOperator('*') })
-    $('#btnDivide').on("click", function () { AddOperator('/') })
-}
-
-function AddNumber(number) {
-    var calculation = GetCalculation();
-
-    if (calculation === "0") {
-        SetCalculation(number);
-    } else {
-        var newCalculation = calculation + number;
-        SetCalculation(newCalculation);
-    }
+var DynamicEventListeners = {
+    RemoveCalculation: function (event) { $(event.target).parent().remove(); }
 };
 
-function AddOperator(operator) {
-    var calculation = GetCalculation();
+var CalculatorUI = function() {
 
-    if (calculation === "0") {
-        return;
+    /* Public functions */
+
+    this.GetCalculation = function () {
+        return GetCalculationPrivate();
     }
 
-    if (IsLastCharacterOperator(calculation)) {
-        return;
-    } else {
-        var newCalculation = calculation + operator;
-        SetCalculation(newCalculation);
-    }
-}
-
-function AddComma() {
-    var calculation = GetCalculation();
-    var lastCharacter = calculation.slice(-1);
-
-    if (IsLastCharacterOperator(lastCharacter)) {
-        return;
+    this.SetCalculation = function (calculation) {
+        SetCalculationPrivate(calculation);
     }
 
-    var newCalculcation = calculation + '.';
-    SetCalculation(newCalculcation);
-}
+    this.SetResult = function (result) {
+        SetResultPrivate(result);
+    }
 
-function AddComment() {
-    var comment = window.prompt('Enter comment:');
+    this.AddCalculationResult = function (result) {
+        var lasku = GetCalculationPrivate();
+        $('#txtResult').val(result);
 
-    if (comment !== undefined &&
-        comment !== "") {
-
-        var lisattava = '<li><div>' + comment + '</div><div class="ui-icon ui-icon-closethick" onclick="RemoveCalculation(event);"></div><div></div></li>'
+        var lisattava = '<li>' +
+                        '<div>' + lasku + ' = ' + result + '</div>' +
+                        '<div class="ui-icon ui-icon-closethick" onclick="DynamicEventListeners.RemoveCalculation(event);"></div>' +
+                        '<div></div>' +
+                        '</li>';
         $('#sortable').append(lisattava)
+
+        this.SetCalculation(0);
     }
-}
 
-function PrintCalculations() {
-    window.print();
-}
+    this.AddComment = function () {
+        var comment = window.prompt('Enter comment:');
 
-function ClearResultsList() {
-    if (window.confirm('Really want to empty all results?')) {
-        $('#sortable').empty();
-    }
-}
+        if (comment !== undefined &&
+            comment !== "") {
 
-function RemoveCalculation(event) {
-    $(event.target).parent().remove();
-}
-
-function DownloadPDF() {
-    var newPdf = new jsPDF();
-
-    var xAxisPosition = 15;
-    var yAxisPosition = 15;
-
-    newPdf.setFontSize(26);
-    newPdf.text(xAxisPosition, yAxisPosition, 'Calculations');
-
-    newPdf.setFontSize(14);
-    newPdf.setFont("courier");
-
-    var counter = 0;
-
-    $.each($('ol#sortable li'), function (index, value) {
-        if (counter > 25) {
-            newPdf.addPage();
-            yAxisPosition = 15;
-            counter = 0;
+            var lisattava = '<li>' + 
+                            '<div>' + comment + '</div>' +
+                            '<div class="ui-icon ui-icon-closethick" onclick="DynamicEventListeners.RemoveCalculation(event);"></div>' +
+                            '<div></div>' +
+                            '</li>';
+            $('#sortable').append(lisattava)
         }
-        counter = counter + 1;
-        yAxisPosition = yAxisPosition + 10;
-        newPdf.text(15, yAxisPosition, $(this).children('div:first-child').text())
-    });
+    }
 
-    newPdf.save('Test.pdf');
+    this.PrintCalculations = function () {
+        window.print();
+    }
+
+    this.ClearAll = function () {
+        SetCalculationPrivate(0);
+        SetResultPrivate(0);
+    }
+
+    this.ClearResultsList = function () {
+        if (window.confirm('Really want to empty all results?')) {
+            $('#sortable').empty();
+            SetCalculationPrivate(0);
+            SetResultPrivate(0);
+        }
+    }
+
+    this.DownloadPDF = function () {
+        var newPdf = new jsPDF();
+
+        var xAxisPosition = 15;
+        var yAxisPosition = 15;
+
+        newPdf.setFontSize(26);
+        newPdf.text(xAxisPosition, yAxisPosition, 'Calculations');
+
+        newPdf.setFontSize(14);
+        newPdf.setFont("courier");
+
+        var counter = 0;
+
+        $.each($('ol#sortable li'), function (index, value) {
+            if (counter > 25) {
+                newPdf.addPage();
+                yAxisPosition = 15;
+                counter = 0;
+            }
+            counter = counter + 1;
+            yAxisPosition = yAxisPosition + 10;
+            newPdf.text(15, yAxisPosition, $(this).children('div:first-child').text())
+        });
+
+        newPdf.save('Test.pdf');
+    }
+
+    /* Private functions */
+
+    var GetCalculationPrivate = function () {
+        return $('#txtCalculation').val();
+    }
+
+    var SetResultPrivate = function (result) {
+        $('#txtResult').val(result);
+    }
+
+    var SetCalculationPrivate = function (calculation) {
+        $('#txtCalculation').val(calculation);
+    }
+
 }
+
+
+var CalculatorFunctions = function(UI) {
+
+    /* Public functions */
+
+    this.AddNumber = function (number) {
+        var calculation = UI.GetCalculation();
+
+        if (calculation === "0") {
+            UI.SetCalculation(number);
+        } else {
+            var newCalculation = calculation + number;
+            UI.SetCalculation(newCalculation);
+        }
+    }
+
+    this.AddOperator = function (operator) {
+        var calculation = UI.GetCalculation();
+
+        if (calculation === "0") {
+            return;
+        }
+
+        if (IsLastCharacterOperator(calculation)) {
+            return;
+        } else {
+            var newCalculation = calculation + operator;
+            UI.SetCalculation(newCalculation);
+        }
+    }
+
+    this.AddComma = function () {
+        var calculation = UI.GetCalculation();
+        var lastCharacter = calculation.slice(-1);
+
+        if (IsLastCharacterOperator(lastCharacter)) {
+            return;
+        }
+
+        var newCalculcation = calculation + '.';
+        UI.SetCalculation(newCalculcation);
+    }
+
+    this.ClearCharacter = function () {
+        var calculation = UI.GetCalculation();
+        
+        if (calculation.length === 0 || calculation.length === 1) {
+            UI.SetCalculation(0);
+        } else {
+            var newCalculation = calculation.substring(0, calculation.length - 1);
+        
+            if (IsLastCharacterOperator(newCalculation)) {
+                newCalculation = calculation.substring(0, newCalculation.length - 1);
+            }
+        
+            UI.SetCalculation(newCalculation);
+        }
+    }
+
+    this.ChangeNumberSign = function() {
+        var calculation = UI.GetCalculation();
+        var operatorPosition = CheckFirstOperatorPosition(calculation);
+        
+        if (operatorPosition === 0) {
+            UI.SetCalculation(-parseFloat(calculation));
+        }
+    }
+
+    this.CalculateTotal = function () {
+        var allCalculations = UI.GetCalculation();
+        var numberOfCalculations = CountNumberOfCalculations(allCalculations);
+        
+        if (numberOfCalculations === 0)
+            return;
+        
+        for (var i = 0; i < numberOfCalculations; i++) {
+            var calculation = TakeCalculation(allCalculations);
+            var result = ExecuteCalculation(calculation);
+            allCalculations = ReplaceCalculationWithResult(allCalculations, calculation, result);
+        }
+        
+        UI.AddCalculationResult(result);
+    }
+
+    /* Private functions */
+
+    var IsLastCharacterOperator = function (string) {
+        if (IsOperator(string[string.length - 1]))
+            return true;
+        return false;
+    }
+
+    var IsOperator = function (character) {
+        if (character == '+' ||
+            character == '-' ||
+            character == '*' ||
+            character == '/') {
+            return true;
+        }
+        return false;
+    }
+
+    var CheckFirstOperatorPosition = function (calculation) {
+        var operatorPositions = [];
+
+        for (var i = 0; i < calculation.length; i++) {
+            if (IsOperator(calculation[i])) {
+                operatorPositions.push(i);
+            }
+        }
+
+        if (operatorPositions.length === 0) {
+            return 0;
+        } else {
+            return operatorPositions[operatorPositions.length - 1]
+        }
+    }
+
+    var ReplaceCalculationWithResult = function (allCalculations, calculation, result) {
+        var poistettavaSetti = calculation["firstNumber"].toString().length +
+                               calculation["operator"].toString().length +
+                               calculation["secondNumber"].toString().length;
+        var newCalculation = allCalculations.substring(poistettavaSetti, allCalculations.length);
+        return result + newCalculation;
+    }
+
+    var CountNumberOfCalculations = function (allCalculations) {
+        var numberOfCalculations = 0;
+        for (var i = 0; i < allCalculations.length; i++) {
+            if (IsOperator(allCalculations[i])) {
+                numberOfCalculations++;
+            }
+        }
+        return numberOfCalculations;
+    }
+
+    var TakeCalculation = function (allCalculations) {
+        var calculation = new Array(3);
+        var firstOperatorIndex = 0;
+        
+        for (var i = 0; i < allCalculations.length; i++) {
+            if (IsOperator(allCalculations[i])) {
+                if (firstOperatorIndex === 0) {
+                    calculation["firstNumber"] = Number(allCalculations.substring(0, i));
+                    calculation["operator"] = allCalculations[i];
+                    firstOperatorIndex = i;
+                } else {
+                    calculation["secondNumber"] = Number(allCalculations.substring(firstOperatorIndex + 1, i));
+                    break;
+                }
+            }
+        }
+          
+        if (calculation["secondNumber"] === undefined) {
+            calculation["secondNumber"] = Number(allCalculations.substring(firstOperatorIndex + 1, i));
+        }
+        
+        return calculation;
+    }
+
+    var ExecuteCalculation = function (calculation) {
+        var operator = calculation["operator"];
+        var firstNumber = Number(calculation["firstNumber"]);
+        var secondNumber = Number(calculation["secondNumber"]);
+        
+        switch (calculation["operator"]) {
+            case '+':
+                return firstNumber + secondNumber;
+                break;
+            case '-':
+                return firstNumber - secondNumber;
+                break;
+            case '*':
+                return firstNumber * secondNumber;
+                break;
+            case '/':
+                return firstNumber / secondNumber;
+                break;
+        }
+    }
+}
+
